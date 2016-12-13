70965c310804bc91f82f21209a3119cb3c697453
AdsonEsteves <shinadson@gmail.com>
2016-12-13 13:24:03 -0200

    DB update 2

--- veiculo.php
+++ veiculo.php
@@ -1,7 +1,7 @@
 <?php
 // Connecting, selecting database
-$dbconn = pg_connect("host=ec2-107-21-212-175.compute-1.amazonaws.com port=5432 dbname=d720m6nvm27rpq user=axiwhaycappayo password=ed2fd1c749c7bdd7ffc48a7338eb48add4948a3dc6f284b682d28b82fe381262 sslmode=require")
-    or die('Could not connect: ' . pg_last_error());
+$dbconn = pg_connect("host=ec2-54-235-123-159.compute-1.amazonaws.com port=5432 dbname=d28gt5qo4kjh76 user=yidwvplyhjyvjz password=fb6dfdd2ec9ecc899b9e459643236be4e3ab165e2adaadcd6ebfc0c7c81cfd97 sslmode=require")
+              or die('Could not connect: ' . pg_last_error());
 
 require 'validador_placa.php';
 
